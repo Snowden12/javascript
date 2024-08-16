3f702859d5d21a49de6d6b7602cd0192e69190b4
Priyanshu Singh <95026352+Snowden12@users.noreply.github.com>
2024-08-16 08:15:02 +0000

    commit change

---
+++ .devcontainer/01_Basics/datatypes.js
@@ -0,0 +1,24 @@
+"use Strict"; //treat all JS code as newer version
+
+//alert(3+3) //we are using nodejs, not the browser
+
+console.log(3+3)
+// code readability should be high
+console.log("Priyanshu")
+
+let name = "Priyanshu "
+let age = 18
+let isLoggedIn = false
+
+//number ==>2 to pwr 53
+//bigint
+//Strin==>""
+//boolean ==> true/false
+//null ->Stand alone value
+//undefined ==> value is not assigned
+// symbol ==> unique
+
+//object
+
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
---
+++ .devcontainer/01_Basics/var.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "priyanshu7815@gmail.com"
+var accountpassword = "12345"
+accountCity = "Kanpur"
+let accountState;
+
+// accountId = 2
+
+
+accountEmail = "hc@hc.com"
+accountpassword = "23232"
+accountCity = "kanpur"
+
+console.log(accountId);
+
+console.table([accountEmail,accountId,accountpassword,accountCity,accountState])
